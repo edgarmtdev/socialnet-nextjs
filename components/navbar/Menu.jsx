8268ce47ee2c2756bb7ef7930d32b4ee05503d97
EdgarMTdev <edgarmontiel961@gmail.com>
2022-06-07 23:35:16 -0500

    add: styled components

---
+++ components/navbar/Menu.jsx
@@ -0,0 +1,24 @@
+import styled, { keyframes, css } from 'styled-components'
+
+const animate = keyframes`
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+`
+
+export const Menu = styled.div`
+    display: none;
+    position: absolute;
+    right: 0px;
+    top: 55px;
+    background-color: black;
+    width: 20%;
+    ${props => props.primary === true && css`
+    animation: ${animate} 1s ease;
+    display: block;
+    `
+    }
+`
--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
 import Link from 'next/link'
 import { signOut } from 'firebase/auth'
-import { useSelector } from 'react-redux'
 import { auth } from '../../config/firebase'
+import { Menu } from './Menu'
 
 export default function Navbar() {
 
     const { logged, user: { name, profilePic } } = useSelector(state => state.auth)
+
+    const [open, setOpen] = useState(false)
 
     return (
         <nav className=' bg-mine-shaft-600 text-white p-4'>
@@ -17,11 +21,20 @@ export default function Navbar() {
                         <li><Link href={'/auth/sigup'}>SignUp</Link></li>
                     </ul>
                     :
-                    <ul className=' flex items-center ml-auto gap-8'>
-                        <li><Link href={'/profile'}>MyProfile</Link></li>
-                        <li className=' cursor-pointer' onClick={() => { signOut(auth) }}>Logout</li>
-                        <li className=' flex gap-2 items-center'><img src={profilePic} alt="" className=' w-6 h-6 rounded-full' />{name}</li>
-                    </ul>
+                    <>
+                        <ul className=' flex items-center ml-auto gap-8'>
+                            <li><Link href={'/profile'}>MyProfile</Link></li>
+                            <li className=' flex gap-2 items-center cursor-pointer' onClick={() => setOpen(!open)}>
+                                <img src={profilePic} alt="" className=' w-6 h-6 rounded-full' />
+                            </li>
+                        </ul>
+                        <Menu primary={open ? true : false}>
+                            <ul className=' flex flex-col'>
+                                <li className=' cursor-pointer hover:bg-mine-shaft-600 py-3 px-5'>{name}</li>
+                                <li className=' cursor-pointer hover:bg-mine-shaft-600 py-3 px-5' onClick={() => { signOut(auth) }}>Logout</li>
+                            </ul>
+                        </Menu>
+                    </>
                 }
             </div>
         </nav>
